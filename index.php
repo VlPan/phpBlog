802c11600d36e4f45cf8d2ffc8fd134b6ee8313d
VlPan <VladimirPant.bel@gmail.com>
2017-03-15 18:53:05 +0300

    little task

--- index.php
+++ index.php
@@ -9,23 +9,33 @@
         echo'не удалось подлючиться к базе данных!<br>';
         echo mysqli_connect_error();
         exit();
-    }
+     }
 
     echo 'Подлючение есть!';
 $result = mysqli_query($connection,"SELECT * FROM `articles_categories`");
 
-?>
+
+if(mysqli_num_rows($result) == 0 ){
+    
+    echo 'записей не найдено!';
+}else{
+
+
+?> 
 
 <ul>
     
     <?php 
         while(($cat = mysqli_fetch_assoc($result))){
-            echo '<li>' . $cat['title'] . '</li>';
+            $articles_count = mysqli_query($connection,'SELECT COUNT(`id`) AS `total_num` FROM `articles` WHERE `categorie_id` =' . $cat['id']);
+            $articles_count_result = mysqli_fetch_assoc($articles_count);
+            echo '<li>' . $cat['title'] . ' (' . $articles_count_result['total_num'] . ')</li>';
         }
      ?>
 </ul>
 
 <?php 
+}
     mysqli_close($connection);
 ?>
 
